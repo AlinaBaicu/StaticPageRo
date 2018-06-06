7ca5ce8f0085f7c8ad80b09e9bd75ff87ff9b554
Alina <alina.baicu93@yahoo.com>
2018-06-06 08:07:06 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* global particlesJS */
+
+AOS.init({
+  duration: 1300,
+  easing: 'linear',
+});
+
+/*particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});*/
